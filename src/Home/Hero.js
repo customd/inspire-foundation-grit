330ff75675dc5bd64b1a8a10dba5809041882c56
Mac Teariki <mac@customd.com>
2020-11-11 10:14:15 +1300

    Update hero image

--- src/Home/Hero.js
+++ src/Home/Hero.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import InteractiveMobile from "../Components/InteractiveMobile";
 import InteractiveDesktop from "../Components/InteractiveDesktop";
+
+function LazyBackground({ src }) {
+  const [source, setSource] = useState("preload.jpg");
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = src;
+    img.onload = () => setSource(src);
+  }, [src]);
+
+  return (
+    <div
+      alt="GRIT logo"
+      className="hero__background"
+      style={{ backgroundImage: `url(${source})` }}
+    ></div>
+  );
+}
 
 const Hero = () => {
   return (
     <section className="hero">
+      <LazyBackground src={"../images/grit-hero.jpg"} />
       <div className="container hero__wrap">
         <div className="hero__content">
           <div className="hero__image">
